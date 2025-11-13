1ab53ac0b1c85f40fbca6d897e5a30bb7e9ccbdf
Toavina <thleong3@gmail.com>
2025-11-13 21:20:14 +0300

    checkpoint utils

---
+++ utils/checkpoint.py
@@ -0,0 +1,30 @@
+from typing import Any, List, Tuple
+
+import numpy as np
+
+
+def save_params(
+    fname: str, idx_to_id: List[Any], embedding: np.ndarray, bias: np.ndarray
+) -> None:
+    with open(fname, "x") as f:
+        f.write("id,embedding,bias\n")
+        for i in range(len(idx_to_id)):
+            id = idx_to_id[i]
+            emb = " ".join([f"{k:.18e}" for k in embedding[i]])
+            b = f"{bias[i]:.18e}"
+            f.write(f"{id},{emb},{b}\n")
+
+
+def load_params(fname: str) -> Tuple[List[Any], np.ndarray, np.ndarray]:
+    idx_to_id, embedding, bias = [], [], []
+    with open(fname, "r") as f:
+        header = f.readline()
+        assert header == "id,embedding,bias", "incorrect header"
+
+        while line := f.readline():
+            id, emb, b = line.split(",")
+            idx_to_id.append(id)
+            embedding.append([float(x) for x in emb.split(" ")])
+            bias.append(float(b))
+
+    return idx_to_id, np.array(embedding), np.array(bias)
