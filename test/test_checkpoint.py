238a7be8b6f4f064dfef664802c7d39016bc782c
Toavina <thleong3@gmail.com>
2025-11-18 11:35:11 +0300

    experiment and hyper param search

--- test/test_checkpoint.py
+++ test/test_checkpoint.py
@@ -9,28 +9,55 @@ from utils.checkpoint import load_params, save_params
 
 @fixture
 def dummy_data():
-    idx_to_id = ["a", "b", "c"]
-    embedding = np.random.rand(3, 2)
-    bias = np.random.rand(3)
+    r_lambda = 0.1
+    r_tau = 0.1
+    r_gamma = 0.1
+    u_idx_to_id = ["a", "b", "c"]
+    m_idx_to_id = ["a", "b", "c"]
+    u_bias = np.random.rand(3)
+    m_bias = np.random.rand(3)
+    u_embedding = np.random.rand(3, 2)
+    m_embedding = np.random.rand(3, 2)
+    f_embedding = np.random.rand(3, 2)
 
-    return (idx_to_id, embedding, bias)
+    return (
+        r_lambda,
+        r_tau,
+        r_gamma,
+        u_idx_to_id,
+        m_idx_to_id,
+        u_bias,
+        m_bias,
+        u_embedding,
+        m_embedding,
+        None,
+        f_embedding,
+    )
 
 
 def test_params_checkpoint(dummy_data):
-    dummy_path = Path("dummy.save")
+    dummy_path = Path("dummy.npz")
 
     if dummy_path.exists():
         os.remove(dummy_path.resolve())
 
-    save_params(str(dummy_path.resolve()), dummy_data[0], dummy_data[1], dummy_data[2])
+    save_params(str(dummy_path.resolve()), *dummy_data)
 
     if not dummy_path.exists():
         raise Exception("No save file created")
 
-    idx_to_id, embedding, bias = load_params(str(dummy_path.resolve()))
+    params = load_params(str(dummy_path.resolve()))
 
-    assert idx_to_id == dummy_data[0], "Idx to Id list inconsistent"
-    assert (embedding == dummy_data[1]).all(), "Embedding array inconsistent"
-    assert (bias == dummy_data[2]).all(), "Bias array inconsistent"
+    assert params["r_lambda"] == dummy_data[0], "Checkpoint Inconsistent"
+    assert params["r_tau"] == dummy_data[1], "Checkpoint Inconsistent"
+    assert params["r_gamma"] == dummy_data[2], "Checkpoint Inconsistent"
+    assert params["idx_to_user_id"] == dummy_data[3], "Checkpoint Inconsistent"
+    assert params["idx_to_movie_id"] == dummy_data[4], "Checkpoint Inconsistent"
+    assert (params["user_bias"] == dummy_data[5]).all(), "Checkpoint Inconsistent"
+    assert (params["movie_bias"] == dummy_data[6]).all(), "Checkpoint Inconsistent"
+    assert (params["user_embedding"] == dummy_data[7]).all(), "Checkpoint Inconsistent"
+    assert (params["movie_embedding"] == dummy_data[8]).all(), "Checkpoint Inconsistent"
+    assert len(params["idx_to_feat_id"]) == 0, "Checkpoint Inconsistent"
+    assert (params["feat_embedding"] == dummy_data[10]).all(), "Checkpoint Inconsistent"
 
     os.remove(dummy_path.resolve())
--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -1,26 +1,60 @@
-from typing import Any, List, Tuple
+import shutil
+from pathlib import Path
+from typing import Any, Dict, List
 
 import numpy as np
 
 
 def save_params(
-    fname: str, idx_to_id: List[Any], embedding: np.ndarray, bias: np.ndarray
-) -> None:
-    with open(fname, "x") as f:
-        for i in range(len(idx_to_id)):
-            id = idx_to_id[i]
-            emb = " ".join([f"{k:.18e}" for k in embedding[i]])
-            b = f"{bias[i]:.18e}"
-            f.write(f"{id},{emb},{b}\n")
+    fname: str,
+    r_lambda: float,
+    r_tau: float,
+    r_gamma: float,
+    idx_to_user_id: List[Any],
+    idx_to_movie_id: List[Any],
+    user_bias: np.ndarray,
+    movie_bias: np.ndarray,
+    user_embedding: np.ndarray,
+    movie_embedding: np.ndarray,
+    idx_to_feat_id: List[Any] | None = None,
+    feat_embedding: np.ndarray | None = None,
+):
+    fpath = Path(fname)
+    fpath.parent.mkdir(parents=True, exist_ok=True)
+
+    if fpath.exists():
+        shutil.rmtree(fpath)
+
+    np.savez(
+        fpath.resolve(),
+        reg=np.array([r_lambda, r_tau, r_gamma]),
+        idx_to_user_id=np.array(idx_to_user_id),
+        idx_to_movie_id=np.array(idx_to_movie_id),
+        user_embedding=user_embedding,
+        movie_embedding=movie_embedding,
+        user_bias=user_bias,
+        movie_bias=movie_bias,
+        idx_to_feat_id=idx_to_feat_id if idx_to_feat_id is not None else np.array([]),
+        feat_embedding=feat_embedding if feat_embedding is not None else np.array([]),
+    )
 
 
-def load_params(fname: str) -> Tuple[List[Any], np.ndarray, np.ndarray]:
-    idx_to_id, embedding, bias = [], [], []
-    with open(fname, "r") as f:
-        while line := f.readline():
-            id, emb, b = line.split(",")
-            idx_to_id.append(id)
-            embedding.append([float(x) for x in emb.split(" ")])
-            bias.append(float(b))
+def load_params(fname: str) -> Dict:
+    fpath = Path(fname)
 
-    return idx_to_id, np.array(embedding), np.array(bias)
+    loaded = np.load(fpath)
+
+    params = dict()
+    params["r_lambda"] = loaded["reg"][0]
+    params["r_tau"] = loaded["reg"][1]
+    params["r_gamma"] = loaded["reg"][2]
+    params["idx_to_user_id"] = loaded["idx_to_user_id"].tolist()
+    params["idx_to_movie_id"] = loaded["idx_to_movie_id"].tolist()
+    params["user_bias"] = loaded["user_bias"]
+    params["movie_bias"] = loaded["movie_bias"]
+    params["user_embedding"] = loaded["user_embedding"]
+    params["movie_embedding"] = loaded["movie_embedding"]
+    params["idx_to_feat_id"] = loaded["idx_to_feat_id"].tolist()
+    params["feat_embedding"] = loaded["feat_embedding"]
+
+    return params
